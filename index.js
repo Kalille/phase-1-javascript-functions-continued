eca176aa5f322034e6c46e4c4385206c30016d58
kalille Overton <>
2021-04-14 13:56:12 -0400

    commit tree b32f2f312e566b6198ebd560e0bbbae37b9e331e

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+function saturdayFun (sat = "roller-skate"){
+    
+    {return (`This Saturday, I want to ${sat}!`)
+}
+}
+function mondayWork (mon = "go to the office"){
+    {return (`This Monday, I will ${mon}.`)
+}
+
+
+}
+ function wrapAdjective (sym ="*"){
+   
+   return function(adj="special"){
+       return (`You are ${sym}${adj}${sym}!`)
+
+
+   }
+  //let results= console.log (`You are ` + `${empahatic}`)
+   
+ } 
+//  let emphatic = (`${sym} ${adj} ${sym}`);
+//  let result = (`You are ${emphatic}`)
